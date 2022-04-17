e413d3323f006bba2d82fcfb60c925b4bbe72d2c
Mark Percy <mark.percy98@outlook.com>
2022-04-17 15:03:42 +0100

    initial setup to project added setup for logging in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FirebaseSetupService } from 'src/app/firebase.setup.service'
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthorisationComponent } from './authorisation/authorisation.component';
+import { LoginComponent } from './authorisation/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorisationComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [FirebaseSetupService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
