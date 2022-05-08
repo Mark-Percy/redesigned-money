618b6cdb1f0f9a38cb107bd64a1f766ed7b09199
Mark Percy <mark.percy98@outlook.com>
2022-05-08 15:39:14 +0100

    Added validation: The form now checks validation on both panes The Passwords now need to match to submit the register form

--- src/app/authorisation/new-user/new-user.component.ts
+++ src/app/authorisation/new-user/new-user.component.ts
@@ -1,36 +1,61 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatTabGroup } from '@angular/material/tabs';
+import { passwordMatch } from 'src/app/form-validation.dirtective';
 import { AutorisationService } from '../authorisation.service';
+
 
 @Component({
 	selector: 'app-new-user',
 	templateUrl: './new-user.component.html',
 	styleUrls: ['./new-user.component.css']
 })
+
+
 export class NewUserComponent implements OnInit {
 
 	@ViewChild('accountTabs') accountTabs!: MatTabGroup;
 
+	selectedTab = new FormControl();
+
 	hide: boolean = true;
 	hideConfirm: boolean = true;
 
-	newAccountForm = this.fb.group({
-		userDetails: this.fb.group({
-			firstName: [''],
-			surname: ['']
-		}),
-		accountDetails: this.fb.group({
-			email: [''],
-			password: [''],
-			confirmPassword: ['']
-		})
-	})
+	newAccountForm!: FormGroup;
 
-  constructor(private authService: AutorisationService, private fb: FormBuilder) { }
+  	constructor(private authService: AutorisationService, private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-  }
-  updateUser() {
-  }
+  	ngOnInit(): void {
+		this.newAccountForm = this.fb.group({
+			userDetails: this.fb.group({
+				firstName: ['', Validators.required],
+				surname: ['', Validators.required]
+			}),
+			accountDetails: this.fb.group({
+				email: ['', Validators.required],
+				password: ['', Validators.required],
+				confirmPassword: ['', Validators.required],
+				
+			},{
+				validators: [passwordMatch],
+				updateOn: 'submit'
+			})
+	
+		});
+	}
+  	updateUser() {
+		if(this.newAccountForm.valid) {
+			console.log('Form Submitted')
+
+		} else {
+			console.log('error')
+		}
+	}
+
+  	moveTab(tabs: number): void {
+		this.newAccountForm.get('userDetails')?.markAllAsTouched()
+		if(this.newAccountForm.get('userDetails')?.valid) {
+			this.selectedTab.setValue(this.selectedTab.value + tabs);
+		}
+	}
 }
