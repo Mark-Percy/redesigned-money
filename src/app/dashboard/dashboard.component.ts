b1c84c32c346c0eb29b8e1ac7dd61352668339b7
Mark Percy <mark.percy98@outlook.com>
2022-08-30 18:34:57 +0100

    Added transactions component Set up layout

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  dashboardCols: number = 2;
+  rowsHeight:string = "vh";
+  constructor(private responsive: BreakpointObserver) {
+    this.responsive.observe([
+      Breakpoints.Small,
+      Breakpoints.Medium,
+
+    ]).subscribe((state: BreakpointState) => {
+      if(state.breakpoints[Breakpoints.Small]){
+        console.log("matches s")
+        this.dashboardCols = 1;
+      }
+      if(state.breakpoints[Breakpoints.Medium]){
+        console.log("matches m")
+        this.dashboardCols = 2;
+      }
+    })
+  }
 
   ngOnInit(): void {
+    
   }
 
 }
