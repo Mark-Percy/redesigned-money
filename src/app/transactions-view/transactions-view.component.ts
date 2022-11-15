3f5fadc211c43dba5d7dc5398b0ef055a5fd1df5
Mark-Percy <37071339+Mark-Percy@users.noreply.github.com>
2022-11-15 16:25:43 +0000

    Added Abilty to change month with arrows to go back and forth (#45)

--- src/app/transactions-view/transactions-view.component.ts
+++ src/app/transactions-view/transactions-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { AddTransactionComponent } from '../add-transaction/add-transaction.component';
 import { TransAccountService } from '../trans-account.service';
@@ -8,21 +8,28 @@ import { TransAccountService } from '../trans-account.service';
   templateUrl: './transactions-view.component.html',
   styleUrls: ['./transactions-view.component.css']
 })
-export class TransactionsViewComponent implements OnInit {
+export class TransactionsViewComponent{
 
   currDate = new Date();
   month;
+  year;
 
   transactions = this.tras.getTransactionsForMonth(this.currDate);
 
   constructor(private tras: TransAccountService, private dialog: MatDialog) {
     this.month = this.currDate.toLocaleString('en-GB',{month:'long'});
-  }
-
-  ngOnInit(): void {
+    this.year = this.currDate.getFullYear();
+    this.currDate.setDate(1)
   }
 
   addTransaction() {
     this.dialog.open(AddTransactionComponent)
+  }
+
+  changeDate(numOfMonths: number) {
+    this.currDate.setMonth(this.currDate.getMonth() + numOfMonths);
+    this.month = this.currDate.toLocaleString('en-GB',{month:'long'});
+    this.transactions = this.tras.getTransactionsForMonth(this.currDate);
+    this.year = this.currDate.getFullYear();
   }
 }
