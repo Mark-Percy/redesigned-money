83a247a56cd8bf3fffcb33fec1b6e17752f1f8b7
Mark Percy <mark.percy98@outlook.com>
2023-02-27 16:11:42 +0000

    Added pots selection to add transaction process

--- src/app/shared/savings.service.ts
+++ src/app/shared/savings.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { collection, collectionData, addDoc, Firestore  } from '@angular/fire/firestore';
+import { collection, collectionData, addDoc, Firestore, doc  } from '@angular/fire/firestore';
+import { runTransaction } from 'firebase/firestore';
 import { Observable } from 'rxjs';
 import { AuthorisationService } from '../authorisation.service';
 import { Pot } from '../dashboard/savings/pots.interface';
@@ -16,8 +17,26 @@ export class SavingsService {
     addDoc(potCollection, potForm)
   }
 
-  getPots(potId: string) {
-    const potCollection = collection(this.fs, 'users/'+this.auth.getUserId()+'/Accounts/'+potId+'/pots');
+  getPots(accountId: string): Observable<Pot[]> {
+    const potCollection = collection(this.fs, 'users/'+this.auth.getUserId()+'/Accounts/'+accountId+'/pots');
     return collectionData(potCollection, {idField: 'id'}) as Observable<Pot[]>
+  }
+
+  async updatePot(potId: String, accountId: String, updAmount: Number) {
+    const potRef = doc(this.fs,`users/${this.auth.getUserId()}/Accounts/${accountId}/pots/${potId}`)
+    const accRef = doc(this.fs,`users/${this.auth.getUserId()}/Accounts/${accountId}`)
+    try {
+      await runTransaction(this.fs, async(transaction) => {
+        const potDoc = await transaction.get(potRef)
+        const accDoc = await transaction.get(accRef)
+        let potAmount = Number((potDoc.data()?.amount + updAmount).toFixed(2))
+        let accAmount = Number((accDoc.data()?.amount + updAmount).toFixed(2))
+        transaction.update(potRef, {amount: potAmount})
+        transaction.update(accRef, {amount: accAmount})
+      })
+      return 1
+    } catch {
+      return 0
+    }
   }
 }
