16afe843e161009b86d393adc025f3f3fd663fd4
Mark Percy <mark.percy98@outlook.com>
2023-02-27 22:07:56 +0000

    Updated header tests to emulate auth service and enable the component test to work

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,14 +1,27 @@
+import { HeaderComponent } from './header.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { AuthorisationService } from '../authorisation.service';
+import { AppModule } from '../app.module';
 
-import { HeaderComponent } from '../app/header/header.component';
+class MockAuthorisationService {
+  user = null;
+}
 
-describe('HeaderComponent', () => {
+describe('When Header Component not logged in', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let loader: HarnessLoader;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      imports:[AppModule],
+      providers: [
+        HeaderComponent, {provide: AuthorisationService, useClass: MockAuthorisationService}
+      ]
+      
     })
     .compileComponents();
   });
@@ -16,10 +29,14 @@ describe('HeaderComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
+
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+
 });
