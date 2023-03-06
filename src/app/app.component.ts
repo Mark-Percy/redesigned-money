14c4aa4645bf7a473082cc33b6fae2596bb64afd
Mark Percy <mark.percy98@outlook.com>
2023-03-06 11:53:40 +0000

    Updated overlay component theming

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component } from '@angular/core';
 
 @Component({
@@ -9,12 +10,18 @@ export class AppComponent {
   title = 'money-manager';
   darkmode:boolean; 
 
-  constructor(){
+  constructor(private overlayContainer: OverlayContainer){
     this.darkmode = localStorage.getItem('dark') == "true" ? true : false;
+    this.overlayContainer.getContainerElement().classList.add(this.darkmode ? 'darkmode': '')
   }
 
   changeThemeMode() {
     this.darkmode = !this.darkmode
+    if(this.darkmode) {
+      this.overlayContainer.getContainerElement().classList.add('darkmode')
+    } else {
+      this.overlayContainer.getContainerElement().classList.remove('darkmode')
+    }
     localStorage.setItem('dark', (this.darkmode ? "true" : "false"));
   }
 }
