8cde052eff0544136004b6596e3afa5ec1158b7e
Mark Percy <mark.percy98@outlook.com>
2024-01-19 14:24:33 +0000

    Added test for service creating

--- src/test/accounts.service.spec.ts
+++ src/test/accounts.service.spec.ts
@@ -2,15 +2,32 @@ import { TestBed } from '@angular/core/testing';
 
 import { AccountsService } from '../app/shared/accounts.service';
 
-describe('AccountsService', () => {
+import { AuthorisationService } from 'src/app/authorisation.service';
+import { Firestore } from '@angular/fire/firestore';
+
+fdescribe('AccountsService', () => {
   let service: AccountsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: Firestore, useClass: MockFirestore},
+        {provide: AuthorisationService, useClass: MockAuthorisationService}
+      ]
+    });
     service = TestBed.inject(AccountsService);
+    
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
+
+class MockFirestore {
+
+}
+
+class MockAuthorisationService {
+
+}
