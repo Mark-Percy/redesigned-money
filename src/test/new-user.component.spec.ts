08a803b74fe7c7f7b678840cc8bb143795cc3a11
Mark Percy <mark.percy98@outlook.com>
2024-01-22 15:25:13 +0000

    Fixed component creation test for new User

--- src/test/new-user.component.spec.ts
+++ src/test/new-user.component.spec.ts
@@ -1,14 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NewUserComponent } from '../app/authorisation/new-user/new-user.component';
+import { AuthorisationService } from 'src/app/authorisation.service';
+import { MatTabsModule } from '@angular/material/tabs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 describe('NewUserComponent', () => {
   let component: NewUserComponent;
   let fixture: ComponentFixture<NewUserComponent>;
+  let MockAuthService: Partial<AuthorisationService>
 
   beforeEach(async () => {
+
+    MockAuthService = {
+
+    }
+    
     await TestBed.configureTestingModule({
-      declarations: [ NewUserComponent ]
+      declarations: [ NewUserComponent ],
+      imports: [
+        MatTabsModule,
+        BrowserAnimationsModule,
+        ReactiveFormsModule,
+        MatIconModule,
+        MatInputModule
+      ],
+      providers: [
+        {provide: AuthorisationService, useValue: MockAuthService}
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +42,7 @@ describe('NewUserComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
