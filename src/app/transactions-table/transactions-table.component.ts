b1b67ff7aae225c0dfbde40c95147723d1b94ce4
Mark Percy <mark.percy98@outlook.com>
2024-05-19 15:40:40 +0100

    Add filter to table proof of concept

--- src/app/transactions-table/transactions-table.component.ts
+++ src/app/transactions-table/transactions-table.component.ts
@@ -1,7 +1,6 @@
 import { Component, Output, EventEmitter, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { TransactionsService } from '../shared/transactions.service';
-import { Observable } from 'rxjs';
-import { DocumentData } from 'firebase/firestore';
+import { Observable, filter, map} from 'rxjs';
 import { TransactionInterface } from '../add-transaction/add-transaction.component';
 import { TitleCasePipe, CurrencyPipe, DatePipe } from '@angular/common';
 import { MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow } from '@angular/material/table';
@@ -31,11 +30,28 @@ export class TransactionsTableComponent{
 
   @Output('openDialog') close: EventEmitter<any> = new EventEmitter<any>();
   @Input('transactions') transactions: Observable<TransactionInterface[]> = this.transactionService.getTransactions(5)
+  enableFilter = false;
+
+  holdTransactions: Observable<TransactionInterface[]> | null = null
 
   displayedColumns: string[] = ['transactionDate','amount', 'category', 'location'];
 
   constructor(public transactionService: TransactionsService) {}
 
+  filterData() {
+    const filterString: string = 'test'
+    // filterString.toLowerCase();
+    this.holdTransactions = this.transactions
+    this.transactions = (this.transactions.pipe(
+      map(items => items.filter(item => item.location == filterString)),
+      filter(items => items && items.length > 0)
+    ))
+  }
+
+  removeFilter() {
+    if(this.holdTransactions) this.transactions = this.holdTransactions
+    this.holdTransactions = null
+  }
 
   openEditDialog(row: any) {
     row.date = row.transactionDate.toDate();
