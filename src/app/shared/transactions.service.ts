cab87375afda7a18d77a7c158606ecff1ebeadb4
Mark Percy <mark.percy98@outlook.com>
2024-10-01 11:12:46 +0100

    Update total on transactions

--- src/app/shared/transactions.service.ts
+++ src/app/shared/transactions.service.ts
@@ -242,6 +242,7 @@ export class TransactionsService {
     if(yearHeld) monthHeld = yearHeld.get(month);
     if(monthHeld) {
       monthHeld.totalAmount = Number((monthHeld.totalAmount + amount).toFixed(2))
+      monthHeld.totalsExcl = Number((monthHeld.totalsExcl + amount).toFixed(2))
       monthHeld.totalTransactions += num
       this.setSubAmounts(category, accountName, amount, frequency, monthHeld)
     }
