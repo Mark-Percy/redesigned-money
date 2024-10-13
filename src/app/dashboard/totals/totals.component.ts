05e22e6375acb61b7e95f73bb3052a535444e7ac
LewBlu <lewis.blundell.development@gmail.com>
2024-10-13 16:00:01 +0100

    Added loading spinner to totals component

--- src/app/dashboard/totals/totals.component.ts
+++ src/app/dashboard/totals/totals.component.ts
@@ -1,41 +1,54 @@
 import { Component, Input } from '@angular/core';
-import { TransactionMonthInterface, TransactionsService } from 'src/app/shared/transactions.service';
+import {
+  TransactionMonthInterface,
+  TransactionsService,
+} from 'src/app/shared/transactions.service';
 import { CurrencyPipe, KeyValuePipe, NgStyle } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
+import { LoadingSpinnerComponent } from '../../shared/components/loading-spinner/loading-spinner.component';
 
 @Component({
-    selector: 'app-totals',
-    templateUrl: './totals.component.html',
-    styleUrls: ['./totals.component.css', '../dashboard_base.css'],
-    standalone: true,
-    imports: [
-      MatIconButton,
-      MatIcon,
-      CurrencyPipe,
-      KeyValuePipe,
-      NgStyle,
-    ]
+  selector: 'app-totals',
+  templateUrl: './totals.component.html',
+  styleUrls: ['./totals.component.css', '../dashboard_base.css'],
+  standalone: true,
+  imports: [
+    MatIconButton,
+    MatIcon,
+    CurrencyPipe,
+    KeyValuePipe,
+    NgStyle,
+    LoadingSpinnerComponent,
+  ],
 })
 export class TotalsComponent {
-  YearsData: Map<number, TransactionMonthInterface>
-
-  yearnum: number = new Date().getFullYear()
-  yearIndex = 0
   @Input() panelWidth = '45vw';
- 
-  constructor(private transactionService: TransactionsService) {
-    this.transactionService.setTransactionsForYear(new Date()).then((transactionsForYear) => {
-      if(transactionsForYear) this.YearsData = transactionsForYear
-    })
 
+  public YearsData: Map<number, TransactionMonthInterface>;
+  public yearnum: number = new Date().getFullYear();
+  public isLoading: boolean = true;
+
+  constructor(private transactionService: TransactionsService) {
+    this.transactionService
+      .setTransactionsForYear(new Date())
+      .then((transactionsForYear) => {
+        this.isLoading = false;
+        if (transactionsForYear) this.YearsData = transactionsForYear;
+      });
   }
   async setYear(num: number) {
-    this.yearnum += num
-    const date = new Date()
-    date.setFullYear(this.yearnum)
-    this.transactionService.setTransactionsForYear(date).then((transactionsForYear) => {
-      if(transactionsForYear) this.YearsData = transactionsForYear
-    })
+    this.isLoading = true;
+    this.yearnum += num;
+    const date = new Date();
+    date.setFullYear(this.yearnum);
+    this.transactionService
+      .setTransactionsForYear(date)
+      .then((transactionsForYear) => {
+        this.isLoading = false;
+        if (transactionsForYear) {
+          this.YearsData = transactionsForYear;
+        }
+      });
   }
 }
