c68fffe605cb0a7621be2f6a8ed61934bb3496f6
Mark-Percy <37071339+Mark-Percy@users.noreply.github.com>
2025-04-23 15:16:53 +0100

    Added tests for login component (#110)
    * Added tests for login component
    
    * Reverted import spacing in login

--- src/app/authorisation/login/login.component.ts
+++ src/app/authorisation/login/login.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } 															from '@angular/core';
-import { FormBuilder, UntypedFormGroup, Validators, FormsModule, ReactiveFormsModule }  from '@angular/forms';
-import { MatButton } 																	from '@angular/material/button';
-import { MatFormField, MatLabel } 														from '@angular/material/form-field';
-import { MatInput } 																	from '@angular/material/input';
-import { Router } 																		from '@angular/router';
-import { AuthError, UserCredential } 													from 'firebase/auth';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, UntypedFormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { AuthError, UserCredential } from 'firebase/auth';
 
-import { AuthorisationService }	from 'src/app/shared/services/authorisation.service';
+import { AuthorisationService } from 'src/app/shared/services/authorisation.service';
 
 @Component({
 	selector: 'app-login',
@@ -49,6 +49,8 @@ export class LoginComponent implements OnInit {
 			this.signInForm.get('password')?.value,
 		).then((user: UserCredential) => {
 				if(user.user.uid) this.router.navigate(['dashboard']);
-		}).catch((error: AuthError) => this.errorMessage = 'Unable to login, please check credentials and try again!');
+		}).catch((error: AuthError) => {
+			this.errorMessage = 'Unable to login, please check credentials and try again!';
+		});
 	}
 }
